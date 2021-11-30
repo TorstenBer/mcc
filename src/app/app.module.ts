0a7d83afc54288bdd70370a0a952d0e50e8ea216
Torsten Lenz <Torsten.Lenz@pinguin.ag>
2021-11-30 13:52:53 +0100

    Revert "added welcome component"
    This reverts commit e07df11839868cb1ead1e4ced4aeec867560da88.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, WelcomeComponent ],
+  declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
